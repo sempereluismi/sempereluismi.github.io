90a6984fa9038cc802ad41c91cf718d3afa5c101
sempereluismi <sempereluismiguel@gmail.com>
2022-11-24 20:34:47 +0000

    prueba 1

---
+++ js/home.e6b82a79.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktarea2_4"]=self["webpackChunktarea2_4"]||[]).push([[177],{9408:function(n,e,t){t.r(e),t.d(e,{default:function(){return i}});var r=t(3396);function u(n,e,t,u,o,a){const c=(0,r.up)("Intro"),i=(0,r.up)("Argumento");return(0,r.wg)(),(0,r.iD)("div",null,[(0,r.Wm)(c),(0,r.Wm)(i)])}var o={name:"HomeView",components:{Intro:(0,r.RC)((()=>t.e(121).then(t.bind(t,3027)))),Argumento:(0,r.RC)((()=>t.e(617).then(t.bind(t,2617))))}},a=t(89);const c=(0,a.Z)(o,[["render",u]]);var i=c}}]);
+//# sourceMappingURL=home.e6b82a79.js.map
